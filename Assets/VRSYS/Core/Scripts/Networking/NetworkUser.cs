ff13312d5a6581fe157a6f20d25d4eb636a89d64
ktn_p <ktnp1204@gmail.com>
2023-05-05 23:36:53 +0200

    Added screen fade to DesktopViewingSetup

--- Assets/VRSYS/Core/Scripts/Networking/NetworkUser.cs
+++ Assets/VRSYS/Core/Scripts/Networking/NetworkUser.cs
@@ -265,13 +265,13 @@ namespace Vrsys
             if (GetComponent<AvatarDesktopAnatomy>() != null)
             {
                 userDisplayGO = PhotonNetwork.Instantiate("UserPrefabs/DesktopUserDisplay", 
-                    NetworkUser.localHead.transform.position + new Vector3(0f, -0.6f, 0f), 
+                    NetworkUser.localHead.transform.position + new Vector3(0f, 0f, 0f), 
                                                             Quaternion.identity);
             }
             else
             {
                 userDisplayGO = PhotonNetwork.Instantiate("UserPrefabs/XRUserDisplay",
-                    NetworkUser.localHead.transform.position + new Vector3(0f, -0.6f, 0f),
+                    NetworkUser.localHead.transform.position + new Vector3(0f, 0f, 0f),
                                                             Quaternion.identity);
             }
 
